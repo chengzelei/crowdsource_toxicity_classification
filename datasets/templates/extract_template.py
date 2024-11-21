004a2ac0284e006de30beb39596bdd5767354b7a
Zelei Cheng <38622719+chengzelei@users.noreply.github.com>
2024-11-20 23:22:15 -0800

    Delete datasets/templates directory

--- datasets/templates/extract_template.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-raw_file = '/home/jys3649/projects/chatguard/datasets/templates/en_templates_mutate_raw_file.csv'
-df = pd.read_csv(raw_file)
-
-#delet rows with 'invalid' as true
-df = df[df['invalid'] != True]
